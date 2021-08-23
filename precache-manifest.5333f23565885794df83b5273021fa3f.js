7063d7d911bb2cfbff1c4ea354f827f30ac7686c
Ivan Contreras <ivanc.contre@gmail.com>
2021-08-23 10:02:00 -0400

    Updates

---
+++ precache-manifest.5333f23565885794df83b5273021fa3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2507c4f79ca76df20f64bfee13571c84",
+    "url": "https://ivancontre.github.io/calendar-app/index.html"
+  },
+  {
+    "revision": "e8e6ebb67738ca4a599c",
+    "url": "https://ivancontre.github.io/calendar-app/static/css/2.f707e5f5.chunk.css"
+  },
+  {
+    "revision": "1bf1666d4a7710029976",
+    "url": "https://ivancontre.github.io/calendar-app/static/css/main.8141b93d.chunk.css"
+  },
+  {
+    "revision": "e8e6ebb67738ca4a599c",
+    "url": "https://ivancontre.github.io/calendar-app/static/js/2.31393b9b.chunk.js"
+  },
+  {
+    "revision": "8caddb37094dce432564aa118134100c",
+    "url": "https://ivancontre.github.io/calendar-app/static/js/2.31393b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf1666d4a7710029976",
+    "url": "https://ivancontre.github.io/calendar-app/static/js/main.ef13d4af.chunk.js"
+  },
+  {
+    "revision": "421eede8b6b24ccbee9c",
+    "url": "https://ivancontre.github.io/calendar-app/static/js/runtime-main.6297062f.js"
+  }
+]);
